a893f39112dde6a451143d455a6161dbda6fe8da
darkflame1312 <hieule4280@gmail.com>
2023-02-11 18:35:47 +0700

    commit homework1

---
+++ Homework 1/src/helloworld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "../inc/print_hieu.h"
+
+int print_hello()
+{
+    printf("hello world!!!");
+    return 0;
+}
---
+++ Homework 1/src/main.c
@@ -0,0 +1,7 @@
+#include "../inc/print_hieu.h"
+
+int main()
+{
+    int a = print_hello();
+    return 0;
+}
